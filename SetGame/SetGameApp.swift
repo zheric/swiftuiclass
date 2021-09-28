4ec54f651616acfc38a150a7cd6618639eafdf47
zheric <zheric@gmail.com>
2021-09-27 23:49:35 -0700

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Hang Zhang on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
